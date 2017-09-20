cb9d5cb50e24589a2faaf37d1f142590789cdab9
Valentin Schneider <valentin.schneider@arm.com>
2017-09-20 15:07:47 +0100

    devfreq_power: Add plot_cdev_states method

--- trappy/devfreq_power.py
+++ trappy/devfreq_power.py
@@ -72,4 +72,77 @@ ftrace dump"""
 
         return pd.DataFrame(self.data_frame["freq"] / 1000000)
 
+    def plot_cdev_states(self, width=None, height=None, xlim="default",
+                         ylim="range", drawstyle="default", devices=None):
+        """Plot the cooling device state evolution
+
+        :param width: The width of the plot
+        :type width: int
+
+        :param height: The height of the plot
+        :type height: int
+
+        :param xlim: The xlim setting of the plot.
+            See :func:`~trappy.plot_utils.set_lim`
+        :type xlim: str or tuple of int
+
+        :param ylim: The ylim setting of the plot
+            See :func:`~trappy.plot_utils.set_lim`
+        :type ylim: str or tuple of int
+
+        :param drawstyle: The drawstyle setting of the plot
+        :type drawstyle: str
+
+        :param devices: List of devfreq devices to plot
+            All are plotted by default
+        :type devices: list of str
+        """
+        from matplotlib import pyplot as plt
+        from trappy.plot_utils import normalize_title, pre_plot_setup, post_plot_setup
+
+        if len(self.data_frame) == 0:
+            raise ValueError("Empty DataFrame")
+
+        thermal_dfr = self.data_frame.copy()
+
+        # Find available devices
+        available_devices = []
+        for device in thermal_dfr["type"].unique().tolist():
+            available_devices.append(device)
+
+        # Sanitize devices
+        if devices is not None:
+
+            thermal_dfr = thermal_dfr[
+                thermal_dfr["type"].isin(devices)
+            ]
+
+            if len(thermal_dfr) == 0:
+                raise ValueError("No {} trace for devices {}".format(self.unique_word, devices))
+
+        cdevs = {}
+        # At this point we've removed the devices we don't care about
+        for device in thermal_dfr["type"].unique().tolist():
+            cdevs[device] = thermal_dfr[thermal_dfr["type"] == device]["cdev_state"]
+
+        # Prepare one subplot per cdev
+        ax = pre_plot_setup(width, height, nrows = len(cdevs))
+
+        several_graphs = len(cdevs) > 1
+
+        for idx, (device, df) in enumerate(cdevs.iteritems()):
+            _ax = ax[idx] if several_graphs else ax
+
+            cdev_title = "Cooling state evolution of {}".format(device)
+            df.plot(ax=_ax, title=cdev_title, drawstyle=drawstyle)
+
+            _ax.set_ylabel('Cooling state')
+            # Add vertical guidelines
+            _ax.yaxis.grid(True)
+            # Ensure tick interval is 1 and not something with weird decimals
+            states = df.unique().tolist()
+            _ax.set_yticks(range(min(states), max(states) + 1))
+
+            post_plot_setup(_ax, xlim=xlim, ylim=ylim)
+
 register_ftrace_parser(DevfreqOutPower, "thermal")
